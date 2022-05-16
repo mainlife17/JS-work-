4bd8f69d28abf12ec2ad758720dde614f5fc603d
main_life <88232917+mainlife17@users.noreply.github.com>
2022-05-16 08:49:26 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let age = +prompt("Сколько Вам лет?");
+
+if (age >= 1 && age <= 10) {
+  alert("MAydacha");
+} else if (age >= 10 && age <= 20) {
+  alert("Kota bacha");
+} else if (age >= 20 && age <= 30) {
+  alert("MArdak");
+} else if (age >= 30 && age <= 50) {
+  alert("Pirshuysi");
+} else if (age >= 50) {
+  alert("PEnsiya");
+}
+    
---
+++ new/nmain.js
@@ -0,0 +1,8 @@
+let mon = +prompt("Какого вы месяца ?");
+if (mon  === 1 && mon ===2) {
+  alert("Zima");
+}
+
+else if (mon = 3 &&  mon = 5  ){
+    alert("VEsna");
+  }
