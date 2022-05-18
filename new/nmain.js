4c1a26ee42ac4ba4d2ab330446b767c8bcafa1bf
main_life <88232917+mainlife17@users.noreply.github.com>
2022-05-18 10:55:33 +0500

    ninet commit

--- new/nmain.js
+++ new/nmain.js
@@ -1,8 +1,10 @@
 let mon = +prompt("Какого вы месяца ?");
-if (mon  === 1 && mon ===2) {
+if ((mon >= 1 && mon < 3) || mon === 12) {
   alert("Zima");
+} else if (mon >= 3 && mon < 6) {
+  alert("VEsna");
+} else if (mon >= 6 && mon < 9) {
+  alert("Leto");
+} else if (mon >= 9 && mon < 12) {
+  alert("Osen");
 }
-
-else if (mon = 3 &&  mon = 5  ){
-    alert("VEsna");
-  }
